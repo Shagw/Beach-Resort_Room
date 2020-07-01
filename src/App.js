0071512e2b6589324d7e066d86de4ae5d177e276
Shashank Agarwal <shashanka141@gmail.com>
2020-07-01 16:02:58 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
-
 import {Route,Switch} from 'react-router-dom';
 import Navbar from './components/Navbar';
 
@@ -13,7 +12,7 @@ function App() {
     <div>
       <Navbar />
       <Switch>
-        <Route exact path="/home" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
         <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
